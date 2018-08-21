cbd6dedc9650e854cd48ecc46a68f33da93e534d
Paulo Eduardo Santos Porto <quantum.paulo@tokiomarine.com.br>
2018-08-21 14:36:59 -0300

    cadastro de equipamento ajuste annotation para pegar do body da requisição

--- src/main/java/br/com/lebraile/controller/EquipamentoController.java
+++ src/main/java/br/com/lebraile/controller/EquipamentoController.java
@@ -3,8 +3,11 @@ package br.com.lebraile.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lebraile.component.EquipamentoComponent;
@@ -22,12 +25,12 @@ public class EquipamentoController {
 	}
 	
 	@PostMapping("/equipamento")
-	public void adicionarEquipamento(Equipamento equipamento) {
+	public void adicionarEquipamento(@RequestBody Equipamento equipamento) {
 		component.adicionarEquipamento(equipamento);
 	}
 	
-	@PostMapping("/equipamento/{id}")
-	public void removerEquipamento(Long id) {
+	@DeleteMapping("/equipamento/{id}")
+	public void removerEquipamento(@PathVariable Long id) {
 		component.removerEquipamento(id);
 	}
 }
